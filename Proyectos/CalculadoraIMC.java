350e4e074db7905547ada444757c604efcd23c8b
FrancoFarias <francofarias386@gmail.com>
2026-02-12 19:49:05 -0300

    Agrego calculadora IMC con validaciones

--- Proyectos/CalculadoraIMC.java
+++ Proyectos/CalculadoraIMC.java
@@ -1,5 +1,50 @@
 package Proyectos;
 
-public class CalculadoraIMC {
+import java.util.Scanner;
 
+public class CalculadoraIMC {
+    public static void main(String[] args) {
+        var consola = new Scanner(System.in);
+        System.out.println("*** Calculadora IMC ***");
+
+        System.out.print("Ingrese su nombre: ");
+        var nombre = consola.nextLine();
+        System.out.print("Ingrese su altura: ");
+        var altura = Double.parseDouble(consola.nextLine());
+        System.out.print("Ingrese su peso: ");
+        var peso = Double.parseDouble(consola.nextLine());
+
+        if (nombre.isEmpty()){
+            System.out.println("Nombre invalido");
+            return;
+        }
+        else if (peso <=0 || peso > 500){
+            System.out.println("Peso invalido");
+            return;
+        }
+        else if (altura>3 || altura <=0){
+            System.out.println("Altura invalida");
+            return;
+        }
+
+        var imc = peso / (altura*altura);
+        String categoria;
+
+        if (imc < 18.5)
+            categoria = "Bajo peso";
+        else if (imc < 25)
+            categoria = "Peso normal";
+        else if (imc < 30)
+            categoria = "Sobrepeso";
+        else
+            categoria = "Obesidad";
+
+
+        System.out.printf("""
+                %nHola %s
+                Tu Imc es: %.2f
+                Categoria: %s
+                """,nombre,imc,categoria);
+
+    }
 }
